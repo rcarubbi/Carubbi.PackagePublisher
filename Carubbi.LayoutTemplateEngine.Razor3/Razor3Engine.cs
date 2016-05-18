602028d832ef805d27fe7ba588983e9fea5ad74d
Raphael Carubbi Neto <rcarubbi@gmail.com>
2016-05-18 13:01:54 -0300

    Update Razor template layout engine

---
+++ Carubbi.LayoutTemplateEngine.Razor3/Razor3Engine.cs
@@ -0,0 +1,30 @@
+﻿using Carubbi.LayoutTemplateEngine.Interfaces;
+using RazorEngine;
+using RazorEngine.Templating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carubbi.LayoutTemplateEngine.Razor3
+{
+    public class Razor3Engine : ILayoutTemplateEngine
+    {
+        public string RenderTemplate(string templateName, IDictionary<string, object> data)
+        {
+            Engine.Razor.AddTemplate(templateName, templateName);
+            // On startup
+            Engine.Razor.Compile(templateName, null);
+            // instead of the Razor.Parse call
+            var result = Engine.Razor.Run(templateName, null, data);
+            return result;
+        }
+
+        public string RenderTemplate(string masterPage, string templateName, IDictionary<string, object> data)
+        {
+            Engine.Razor.AddTemplate(masterPage, masterPage);
+            return RenderTemplate(templateName, data);
+        }
+    }
+}
