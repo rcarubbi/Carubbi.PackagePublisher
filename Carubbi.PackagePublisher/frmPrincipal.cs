a3d74472a11654e63ed3e515ad68e3c428c8f557
Raphael Carubbi <rcarubbi@gmail.com>
2018-08-22 18:01:53 +0100

    resharper refactoring

--- Carubbi.PackagePublisher/frmPrincipal.cs
+++ Carubbi.PackagePublisher/frmPrincipal.cs
@@ -1,9 +1,9 @@
-﻿using Carubbi.PackagePublisher.Properties;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using Carubbi.PackagePublisher.Properties;
 
 namespace Carubbi.PackagePublisher
 {
@@ -16,23 +16,16 @@ namespace Carubbi.PackagePublisher
 
         private void btnPublicar_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < clbPacotes.Items.Count; i++)
+            for (var i = 0; i < clbPacotes.Items.Count; i++)
             {
-                if (clbPacotes.GetItemChecked(i))
-                {
-                    var projectName = clbPacotes.Items[i].ToString();
-                    var entries = Directory.GetFiles(txtCaminhoPacotes.Text, string.Format("{0}.csproj", projectName), SearchOption.AllDirectories);
-                    if (entries.Length > 0)
-                    {
-                        Publicar(entries[0]);
-                     
-                    }
-
-                }
+                if (!clbPacotes.GetItemChecked(i)) continue;
+                var projectName = clbPacotes.Items[i].ToString();
+                var entries = Directory.GetFiles(txtCaminhoPacotes.Text, $"{projectName}.csproj",
+                    SearchOption.AllDirectories);
+                if (entries.Length > 0) Publicar(entries[0]);
             }
 
             MessageBox.Show($@"Publicação concluída!");
-
         }
 
         private void Publicar(string projeto)
@@ -49,35 +42,27 @@ namespace Carubbi.PackagePublisher
                     CreateNoWindow = true,
                     FileName = "nuget.exe",
                     RedirectStandardInput = true,
-                    Arguments = string.Format("pack \"{0}\" -IncludeReferencedProjects -Properties Configuration=Release", fi.FullName)
+                    Arguments = $"pack \"{fi.FullName}\" -IncludeReferencedProjects -Properties Configuration=Release"
                 }
             };
             var lines = new List<string>();
             var errorsLines = new List<string>();
             p.Start();
-            while (!p.StandardOutput.EndOfStream)
-            { 
-                lines.Add(p.StandardOutput.ReadLine());
-            }
+            while (!p.StandardOutput.EndOfStream) lines.Add(p.StandardOutput.ReadLine());
 
-            while (!p.StandardError.EndOfStream)
-            {
-                errorsLines.Add(p.StandardError.ReadLine());
-            }
+            while (!p.StandardError.EndOfStream) errorsLines.Add(p.StandardError.ReadLine());
 
 
             p.WaitForExit();
 
-            if (errorsLines.Count == 0)
-            {
-                var pacote = Directory.GetFiles(Application.StartupPath, "*.nupkg")[0];
-                var destino = Path.Combine(@"C:\Users\RaphaelCarubbi\Documents\Components", Path.GetFileName(pacote));
-                if (File.Exists(destino))
-                    File.Delete(destino);
+            if (errorsLines.Count != 0) return;
+            var pacote = Directory.GetFiles(Application.StartupPath, "*.nupkg")[0];
+            var destino = Path.Combine(Application.StartupPath, Path.GetFileName(pacote));
+            if (File.Exists(destino))
+                File.Delete(destino);
 
-                File.Copy(pacote, destino);
-                File.Delete(pacote);
-            }
+            File.Copy(pacote, destino);
+            File.Delete(pacote);
         }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
@@ -91,31 +76,23 @@ namespace Carubbi.PackagePublisher
         {
             var projects = Directory.GetFiles(txtCaminhoPacotes.Text, "*.nuspec", SearchOption.AllDirectories);
 
-            foreach (string project in projects)
-            {
-                clbPacotes.Items.Add(Path.GetFileNameWithoutExtension(project));
-            }
+            foreach (var project in projects) clbPacotes.Items.Add(Path.GetFileNameWithoutExtension(project));
         }
+
         private void btnAtualizar_Click(object sender, EventArgs e)
         {
             var result = fdCaminhoBase.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-          
-                txtCaminhoPacotes.Text = fdCaminhoBase.SelectedPath;
-                Settings.Default["UltimaPastaUtilizada"] = txtCaminhoPacotes.Text;
-                Settings.Default.Save();
-                ListarPacotes();
-            }
+            if (result != DialogResult.OK) return;
+            txtCaminhoPacotes.Text = fdCaminhoBase.SelectedPath;
+            Settings.Default["UltimaPastaUtilizada"] = txtCaminhoPacotes.Text;
+            Settings.Default.Save();
+            ListarPacotes();
         }
 
         private void chkTodos_CheckedChanged(object sender, EventArgs e)
         {
-            for (int i = 0; i < clbPacotes.Items.Count; i++)
-            {
+            for (var i = 0; i < clbPacotes.Items.Count; i++)
                 clbPacotes.SetItemCheckState(i, chkTodos.Checked ? CheckState.Checked : CheckState.Unchecked);
-            }
-            
         }
     }
 }
